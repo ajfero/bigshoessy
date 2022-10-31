0248cffefb5afe105032b2de505ec5a8ea53b374
ajfero <ajfero.dev@gmail.com>
2022-10-31 14:27:23 -0300

    build: create service for messages on view contactUs. update component contact, and create Interface Message Model.

--- src/app/modules/views/views/contact/contact.component.ts
+++ src/app/modules/views/views/contact/contact.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+// Model
+import { Message, CreateMessageDTO } from '../../models/message.model';
+
+// Service
+import { MessageService } from '../../services/message.service';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  //variables
+  messages: Message[] = [];
 
-  ngOnInit(): void {
+  constructor(
+
+    private messageService: MessageService,
+
+  ) { }
+
+  ngOnInit(): void { }
+
+  createNewMessage() {
+
+    const message: CreateMessageDTO = {
+      name: "Tomas",
+      email: "Tomas@user.com",
+      subject: "Titulo del mensaje enviado por Tomas",
+      message: "Mensaje enviado por Tomas, desde la vista contactos; Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"
+    }
+
+    this.messageService.postMessage(message)
+      .subscribe(message => {
+        this.messages.unshift(message);
+      });
+
   }
-
 }
